8045ca112a88f57454e51081690f91be1c8d041e
DatDCao <docaodat432001@gmail.com>
2021-12-26 20:04:45 +0700

    push lan 1

---
+++ js/item.js
@@ -0,0 +1,8 @@
+function open_Form() {
+  document.getElementById("myForm").style.opacity = "1";
+  document.getElementById("myForm").style.transform = "translateY(0)";
+}
+function closeForm() {
+  document.getElementById("myForm").style.opacity = "0";
+  document.getElementById("myForm").style.transform = "translateY(800px)";
+}
